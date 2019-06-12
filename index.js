9a1a8065ae3aa01fdcc7e5133058f3c881d388ba
Deep Chakraborty <deepc.au@gmail.com>
2019-06-12 20:01:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var a="Hello",
     sp=" ",
     fs=".";
 
-var greeting = a+comma+sp+who+ex;
-var specialGuest = "Neil deGrasse Tyson"
+var greeting = a+comma+sp+who+ex,
+    specialGuest = "Neil deGrasse Tyson";
 var greetSpecialGuest = a+comma+sp + specialGuest + "!";
 var conversation = "Let's talk about"+sp+`${topic}`+fs;
